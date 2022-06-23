04ddffff2ca115c3fa8c718259743d3e169522ab
Kevin Canico <kevcanico25@gmail.com>
2022-06-23 19:28:37 +0200

    :sparkles: add and remove questionnaire in list

--- src/views/Questionnaires.vue
+++ src/views/Questionnaires.vue
@@ -7,8 +7,11 @@
       <span class="slider round"></span>
     </label>
     <ul>
+      <li><button @click="create">Créer un questionnaire</button></li>
       <li v-for="questionnaire_model in questionnaire_models" :key="questionnaire_model.id">
-        <router-link :to="'/questionnaire/' + questionnaire_model.id">{{ questionnaire_model.name }} ({{questionnaire_model.is_active ? 'Activé' : 'Désactivé'}})</router-link>
+        <p>{{ questionnaire_model.name }} ({{questionnaire_model.is_active ? 'Activé' : 'Désactivé'}})</p>
+        <router-link :to="'/questionnaire/' + questionnaire_model.id">Editer</router-link>
+        <button @click="destroy(questionnaire_models.id)">Supprimer</button>
       </li>
     </ul>
   </div>
@@ -50,6 +53,22 @@ export default {
           .then((response) => {
             this.$store.setShop(response.data)
           })
+    },
+    async create () {
+      return await axios
+          .post('questionnaire_models', {
+            'name': 'Questionnaire',
+            'is_active': false,
+            'shop_id': this.shop.id
+          })
+          .then((response) => {
+            this.questionnaire_models.push(response.data)
+          })
+    },
+    async destroy (questionnaire_model_id) {
+      return await axios
+          .delete('questionnaire_models/' + questionnaire_model_id)
+          .then(() => {this.questionnaire_models = this.questionnaire_models.map(questionnaire_model => questionnaire_model.id !== questionnaire_model_id)})
     }
   },
   mounted() {
