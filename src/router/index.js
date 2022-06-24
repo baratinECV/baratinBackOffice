97188bb018c12757fa9523129023288ba581f777
Fabrice Cottin <fabrice.cottin1@viacesi.fr>
2022-06-24 02:33:05 +0200

    creation de la page tags

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Products from '../views/Products.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Products from "../views/Products.vue";
 import Favorites from "../views/Favorites";
 import Questionnaires from "../views/Questionnaires";
 import Questionnaire from "../views/Questionnaire";
+import Tags from "../views/Tags.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Products',
-    component: Products
+    path: "/",
+    name: "Products",
+    component: Products,
   },
   {
-    path: '/products',
-    name: 'Products',
-    component: Products
+    path: "/products",
+    name: "Products",
+    component: Products,
   },
   {
-    path: '/favorites',
-    name: 'Favorites',
-    component: Favorites
+    path: "/favorites",
+    name: "Favorites",
+    component: Favorites,
   },
   {
-    path: '/questionnaires',
-    name: 'Questionnaires',
-    component: Questionnaires
-  }
-  ,
+    path: "/questionnaires",
+    name: "Questionnaires",
+    component: Questionnaires,
+  },
   {
-    path: '/questionnaire/:id',
-    name: 'Questionnaire',
+    path: "/questionnaire/:id",
+    name: "Questionnaire",
     component: Questionnaire,
-    props: true
-  }
-]
+    props: true,
+  },
+  {
+    path: "/tags",
+    name: "Tags",
+    component: Tags,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
